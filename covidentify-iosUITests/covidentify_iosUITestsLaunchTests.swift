010ca505388fe2eb5fc7612d0e2d4d5d08c32709
Shun Sakai <ss1184@duke.edu>
2022-10-04 17:22:06 -0400

    Initial Commit

---
+++ covidentify-iosUITests/covidentify_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  covidentify_iosUITestsLaunchTests.swift
+//  covidentify-iosUITests
+//
+//  Created by Shun Sakai on 10/4/22.
+//
+
+import XCTest
+
+class covidentify_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
