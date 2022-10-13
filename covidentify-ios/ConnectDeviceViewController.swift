dcc68270c5048e532f97a279bfa7b959abe23ba7
james <jingxing.wang@duke.edu>
2022-10-13 10:32:57 -0400

    basic storyboard layout

---
+++ covidentify-ios/ConnectDeviceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConnectDeviceViewController.swift
+//  covidentify-ios
+//
+//  Created by James Wang on 10/13/22.
+//
+
+import UIKit
+
+class ConnectDeviceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
