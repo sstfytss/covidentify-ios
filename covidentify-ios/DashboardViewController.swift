71870b3885dd045d14a184155583599c44f74fe0
james <jingxing.wang@duke.edu>
2022-11-29 21:06:46 -0500

    add post method for step count and sleep

--- covidentify-ios/DashboardViewController.swift
+++ covidentify-ios/DashboardViewController.swift
@@ -26,10 +26,11 @@ class DashboardViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: Any) {
         healthStore = HKHealthStore()
 //        queryHeartRate()
-        queryStepCount()
+//        queryStepCount()
 //        querySleepAnalysis()
     
     }
+    
     func postJsonHeartRateData(jsonString: String) {
         guard let url = URL(string: "https://ios-http-db.azurewebsites.net/api/HttpTrigger-ios"),
               let payload = jsonString.data(using: .utf8)
@@ -56,14 +57,13 @@ class DashboardViewController: UIViewController {
         
         
     }
-    func postHeartRateData(participantId: Int, deviceId: Int, date: String, heartRate: Int) {
-        guard let url = URL(string: "http://test-ios.azurewebsites.net/api/todo"),
-              let payload = "{\"id\":0,\"participant_id\":\(participantId),\"device_id\":\(deviceId),\"date\":\"\(date)\",\"heart_rate\":\(heartRate)}".data(using: .utf8)
+    func postJsonStepCountData(jsonString: String) {
+        guard let url = URL(string: "https://ios-http-db.azurewebsites.net/api/HttpTrigger-ios"),
+              let payload = jsonString.data(using: .utf8)
         else {
-            print("returning")
+            print("URL error")
             return
         }
-        print("returning2")
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("text/plain", forHTTPHeaderField: "accept")
@@ -83,7 +83,33 @@ class DashboardViewController: UIViewController {
         
         
     }
-      
+    func postJsonSleepData(jsonString: String) {
+        guard let url = URL(string: "https://ios-http-db.azurewebsites.net/api/HttpTrigger-ios"),
+              let payload = jsonString.data(using: .utf8)
+        else {
+            print("URL error")
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("text/plain", forHTTPHeaderField: "accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = payload
+        request.timeoutInterval = 1000
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print("request, \(response)")
+            guard error == nil else { print(error!.localizedDescription); return }
+            guard let data = data else { print("Empty data"); return }
+
+            if let str = String(data: data, encoding: .utf8) {
+                print(str)
+            }
+        }.resume()
+        
+        
+    }
+
     
     func querySleepAnalysis() -> Void {
         guard let sampleSleep = HKSampleType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) else {
@@ -97,10 +123,34 @@ class DashboardViewController: UIViewController {
                 print("sleep analysis error")
                 return
             }
+            var myNewDictArray: [Dictionary<String, String>] = []
+            
             for sample in samples {
                 // Process each sample here.
-                print("Sleep value: " + "\(sample.value)" + " Start Time: " + "\(sample.startDate)" + " End Time: " + "\(sample.endDate)")
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+                
+                if (sample.device?.hardwareVersion == "Watch6,6" || sample.device?.hardwareVersion == "Watch6,7" || sample.device?.hardwareVersion == "Watch6,8" || sample.device?.hardwareVersion == "Watch6,9") {
+                    if (myNewDictArray.count == 0) {
+                        var deviceType: [String:String] = ["apple_watch_type": "series_7"]
+                        myNewDictArray.append(deviceType)
+                    }
+                    var dictEntry: [String:String] = ["participant_id":"8888","device_id": "6666", "start_time":formatter.string(from: sample.startDate), "end_time":formatter.string(from: sample.endDate)]
+                    myNewDictArray.append(dictEntry)
+                    
+                }
+
             }
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: myNewDictArray, options: [])
+                let theJSONText = String(data: jsonData, encoding: .ascii)
+                    print("JSON string = \(theJSONText!)")
+                self.postJsonSleepData(jsonString: theJSONText!)
+                
+            } catch {
+                print("error in converting data to json")
+            }
+            
             
             // The results come back on an anonymous background queue.
             // Dispatch to the main queue before modifying the UI.
@@ -133,15 +183,41 @@ class DashboardViewController: UIViewController {
                 return
             }
             
+            var myNewDictArray: [Dictionary<String, String>] = []
+            
             for sample in samples {
                 // Process each sample here.
-                if let device = sample.device {
-                    print(device)
+                //print("Step quantity: " + "\(sample.quantity)" + " Start Time: " + "\(sample.startDate)" + " End Time: " + "\(sample.endDate)")
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+                
+                if (sample.device?.hardwareVersion == "Watch6,6" || sample.device?.hardwareVersion == "Watch6,7" || sample.device?.hardwareVersion == "Watch6,8" || sample.device?.hardwareVersion == "Watch6,9") {
+                    if (myNewDictArray.count == 0) {
+                        var deviceType: [String:String] = ["apple_watch_type": "series_7"]
+                        myNewDictArray.append(deviceType)
+                    }
+                    var dictEntry: [String:String] = ["participant_id":"8888","device_id": "6666",  "step_count":"\(Int(sample.quantity.doubleValue(for: HKUnit.count())))", "start_time":formatter.string(from: sample.startDate), "end_time":formatter.string(from: sample.endDate)]
+                    myNewDictArray.append(dictEntry)
+                    
                 }
-                print("Step quantity: " + "\(sample.quantity)" + " Start Time: " + "\(sample.startDate)" + " End Time: " + "\(sample.endDate)")
 
             }
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: myNewDictArray, options: [])
+                let theJSONText = String(data: jsonData, encoding: .ascii)
+                    print("JSON string = \(theJSONText!)")
+                self.postJsonStepCountData(jsonString: theJSONText!)
+                
+            } catch {
+                print("error in converting data to json")
+            }
             
+            
+            
+            
+            
+            
+      
             // The results come back on an anonymous background queue.
             // Dispatch to the main queue before modifying the UI.
             
@@ -173,9 +249,17 @@ class DashboardViewController: UIViewController {
 //                print("Heart Rate: " + "\(sample.quantity)" + " Start Time: " + "\(sample.startDate)" + " End Time: " + "\(sample.endDate)")
                 let formatter = DateFormatter()
                 formatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
-//                self.postHeartRateData(participantId: 0, deviceId: 0, date: formatter.string(from: sample.startDate), heartRate: Int(sample.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))))
-                var dictEntry: [String:String] = ["device_id": "6666",  "date":formatter.string(from: sample.startDate), "heart_rate":"\(Int(sample.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))))", "participant_id":"8888"]
-                myNewDictArray.append(dictEntry)
+                
+                if (sample.device?.hardwareVersion == "Watch6,6" || sample.device?.hardwareVersion == "Watch6,7" || sample.device?.hardwareVersion == "Watch6,8" || sample.device?.hardwareVersion == "Watch6,9") {
+                    if (myNewDictArray.count == 0) {
+                        var deviceType: [String:String] = ["apple_watch_type": "series_7"]
+                        myNewDictArray.append(deviceType)
+                    }
+                    var dictEntry: [String:String] = ["device_id": "6666",  "date":formatter.string(from: sample.startDate), "heart_rate":"\(Int(sample.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))))", "participant_id":"8888"]
+                    myNewDictArray.append(dictEntry)
+                    
+                }
+
             }
             do {
                 let jsonData = try JSONSerialization.data(withJSONObject: myNewDictArray, options: [])
@@ -198,56 +282,56 @@ class DashboardViewController: UIViewController {
         healthStore!.execute(queryHeartRate)
     }
     
-    func queryActivity() -> Void {
-        
-        let calendar = NSCalendar.current
-        let endDate = Date()
-         
-        guard let startDate = calendar.date(byAdding: .day, value: -7, to: endDate) else {
-            fatalError("*** Unable to create the start date ***")
-        }
-
-        let units: Set<Calendar.Component> = [.day, .month, .year, .era]
-
-        var startDateComponents = calendar.dateComponents(units, from: startDate)
-        startDateComponents.calendar = calendar
-
-        var endDateComponents = calendar.dateComponents(units, from: endDate)
-        endDateComponents.calendar = calendar
-
-        // Create the predicate for the query
-        let summariesWithinRange = HKQuery.predicate(forActivitySummariesBetweenStart: startDateComponents, end: endDateComponents)
-        
-        
-        
-        let query = HKActivitySummaryQuery(predicate: summariesWithinRange) { (query, summariesOrNil, errorOrNil) -> Void in
-            
-            guard let summaries = summariesOrNil else {
-                // Handle any errors here.
-                print("no summary")
-                return
-            }
-            
-            for summary in summaries {
-                // Process each summary here.
-                print(summary)
-            }
-            
-            // The results come back on an anonymous background queue.
-            // Dispatch to the main queue before modifying the UI.
-            
-            DispatchQueue.main.async {
-                // Update the UI here.
-            }
-        }
-        healthStore!.execute(query)
-    }
+//    func queryActivity() -> Void {
+//
+//        let calendar = NSCalendar.current
+//        let endDate = Date()
+//
+//        guard let startDate = calendar.date(byAdding: .day, value: -7, to: endDate) else {
+//            fatalError("*** Unable to create the start date ***")
+//        }
+//
+//        let units: Set<Calendar.Component> = [.day, .month, .year, .era]
+//
+//        var startDateComponents = calendar.dateComponents(units, from: startDate)
+//        startDateComponents.calendar = calendar
+//
+//        var endDateComponents = calendar.dateComponents(units, from: endDate)
+//        endDateComponents.calendar = calendar
+//
+//        // Create the predicate for the query
+//        let summariesWithinRange = HKQuery.predicate(forActivitySummariesBetweenStart: startDateComponents, end: endDateComponents)
+//
+//
+//
+//        let query = HKActivitySummaryQuery(predicate: summariesWithinRange) { (query, summariesOrNil, errorOrNil) -> Void in
+//
+//            guard let summaries = summariesOrNil else {
+//                // Handle any errors here.
+//                print("no summary")
+//                return
+//            }
+//
+//            for summary in summaries {
+//                // Process each summary here.
+//                print(summary)
+//            }
+//
+//            // The results come back on an anonymous background queue.
+//            // Dispatch to the main queue before modifying the UI.
+//
+//            DispatchQueue.main.async {
+//                // Update the UI here.
+//            }
+//        }
+//        healthStore!.execute(query)
+//    }
+//
     
-    
-    @IBAction func clickBlob(_ sender: Any) {
-        
-        
-    }
+//    @IBAction func clickBlob(_ sender: Any) {
+//
+//
+//    }
     
  
 }
